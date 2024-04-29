a028ce4b8791ba06d80eecfd6acb09b32ac26d7e
AbhinavThella <94911089+AbhinavThella@users.noreply.github.com>
2024-04-29 17:48:54 +0530

    Delete Project 5(BURGER PRINCE) directory

--- Project 5(BURGER PRINCE)/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementsByClassName('header-hamburger')[0]
-const navbar = document.getElementsByClassName('header-list')[0]
-
-hamburger.addEventListener('click', () => {
-    navbar.classList.toggle('active')
-})
