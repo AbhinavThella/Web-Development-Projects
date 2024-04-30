09d2cb67bc1460615815a78f5e45d9050a2b1dac
AbhinavThella <94911089+AbhinavThella@users.noreply.github.com>
2024-04-30 23:43:03 +0530

    Delete Portfolio-2 directory

--- Portfolio-2/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementsByClassName('hamburger')[0]
-const navbar = document.getElementsByClassName('nav-links')[0]
-
-hamburger.addEventListener('click', () => {
-    navbar.classList.toggle('active')
-})
